aa435a6a0a3762cca709b40151bdb7901c600268
g0rbe <mail@g0rbe.xyz>
2019-09-06 15:32:47 +0200

    update + add IsAddr

--- chattr.go
+++ chattr.go
@@ -21,7 +21,6 @@ package chattr
 
 import (
 	"os"
-	"runtime"
 	"syscall"
 	"unsafe"
 )
@@ -30,6 +29,7 @@ import (
 File attributes.
 */
 const (
+	// from /usr/include/linux/fs.h
 	FS_SECRM_FL        = 0x00000001 /* Secure deletion */
 	FS_UNRM_FL         = 0x00000002 /* Undelete */
 	FS_COMPR_FL        = 0x00000004 /* Compress file */
@@ -41,7 +41,7 @@ const (
 	FS_DIRTY_FL        = 0x00000100
 	FS_COMPRBLK_FL     = 0x00000200 /* One or more compressed clusters */
 	FS_NOCOMP_FL       = 0x00000400 /* Don't compress */
-	FS_ECOMPR_FL       = 0x00000800 /* Compression error */
+	FS_ENCRYPT_FL      = 0x00000800 /* Encrypted file */
 	FS_BTREE_FL        = 0x00001000 /* btree format dir */
 	FS_INDEX_FL        = 0x00001000 /* hash-indexed directory */
 	FS_IMAGIC_FL       = 0x00002000 /* AFS directory */
@@ -49,38 +49,25 @@ const (
 	FS_NOTAIL_FL       = 0x00008000 /* file tail should not be merged */
 	FS_DIRSYNC_FL      = 0x00010000 /* dirsync behaviour (directories only) */
 	FS_TOPDIR_FL       = 0x00020000 /* Top of directory hierarchies*/
+	FS_HUGE_FILE_FL    = 0x00040000 /* Reserved for ext4 */
 	FS_EXTENT_FL       = 0x00080000 /* Extents */
-	FS_DIRECTIO_FL     = 0x00100000 /* Use direct i/o */
+	FS_EA_INODE_FL     = 0x00200000 /* Inode used for large EA */
+	FS_EOFBLOCKS_FL    = 0x00400000 /* Reserved for ext4 */
 	FS_NOCOW_FL        = 0x00800000 /* Do not cow file */
+	FS_INLINE_DATA_FL  = 0x10000000 /* Reserved for ext4 */
 	FS_PROJINHERIT_FL  = 0x20000000 /* Create with parents projid */
 	FS_RESERVED_FL     = 0x80000000 /* reserved for ext2 lib */
+
 )
 
 /*
-Request flags differs on 32/64 bit arhitecture.
-These variables are assigned on runtime.
+Request flags.
 */
-var (
-	FS_IOC_GETFLAGS uintptr
-	FS_IOC_SETFLAGS uintptr
+const (
+	// from ioctl_list manpage
+	FS_IOC_GETFLAGS uintptr = 0x80086601
+	FS_IOC_SETFLAGS uintptr = 0x40086602
 )
-
-/*
-Change between x86-64 and x86-32.
-*/
-func init() {
-
-	if runtime.GOARCH == "amd64" {
-
-		FS_IOC_GETFLAGS = uintptr(0x80086601)
-		FS_IOC_SETFLAGS = uintptr(0x40086602)
-
-	} else if runtime.GOARCH == "386" {
-
-		FS_IOC_GETFLAGS = uintptr(0x80046601)
-		FS_IOC_SETFLAGS = uintptr(0x40046602)
-	}
-}
 
 func ioctl(f *os.File, request uintptr, attrp *int32) error {
 
@@ -98,7 +85,7 @@ func ioctl(f *os.File, request uintptr, attrp *int32) error {
 /*
 GetAttr retrieves the attributes of a file.
 */
-func GetAttr(f *os.File) (int32, error) {
+func GetAttrs(f *os.File) (int32, error) {
 
 	attr := int32(-1)
 
@@ -108,34 +95,53 @@ func GetAttr(f *os.File) (int32, error) {
 }
 
 /*
-SetAttr sets the attribute of a file.
+SetAttr sets the given attribute.
 */
 func SetAttr(f *os.File, attr int32) error {
 
-	attrs, err := GetAttr(f)
+	attrs, err := GetAttrs(f)
 
 	if err != nil {
 		return err
 	}
 
-	attrs = attrs + attr
+	attrs |= attr
 
 	return ioctl(f, FS_IOC_SETFLAGS, &attrs)
 
 }
 
 /*
-UnsetAttr unsets the attribute of a file.
+UnsetAttr unsets the given attribute.
 */
 func UnsetAttr(f *os.File, attr int32) error {
 
-	attrs, err := GetAttr(f)
+	attrs, err := GetAttrs(f)
 
 	if err != nil {
 		return err
 	}
 
-	attrs = attrs - attr
+	attrs ^= attr
 
 	return ioctl(f, FS_IOC_SETFLAGS, &attrs)
+}
+
+/*
+IsAttr checks whether the given attribute is set.
+*/
+func IsAttr(f *os.File, attr int32) (bool, error) {
+
+	attrs, err := GetAttrs(f)
+
+	if err != nil {
+		return false, err
+	}
+
+	if (attrs & attr) != 0 {
+		return true, nil
+	} else {
+		return false, nil
+	}
+
 }
